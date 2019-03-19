460335bb0e59da2a85867dca2ea7e540feb69b27
Vytas88 <vytas.tenys@gmail.com>
2019-03-19 21:25:13 +0200

    Last one

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -21,5 +21,11 @@ $(function(){
   $('#burger').on('click', function(){
     $('#side-menu').toggleClass('side-menu--closed');
     $('#section-container').toggleClass('section-container--closed');
+    $('body').toggleClass('sidenav-closed');
   })
+
+var basename = $('#side-menu').find('li.active span').text();
+  $('#basename') 
+  .text(basename)
+  .attr('href', '/${basename.toLowerCase()}.html'); //prideda atributa //title.hbs ziureti
 })
